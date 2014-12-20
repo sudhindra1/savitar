41b1795f91bcfb53a72b57f2379d031b6fde2c21
Sudhindra <sudhindra@robosoftin.com>
2014-12-20 12:01:50 +0530

    This is test commit

---
+++ aTest.js
@@ -0,0 +1,16 @@
+var namespace = namespace || {}
+(function() {
+    var aTest = function() {
+    }
+    aTest.prototype.constructor = aTest;
+    aTst.prototype = Object.create(namespace.zTest.prototype);
+    aTst.prototype.systemTest = function() {
+        return this.string;
+    }
+    namespace.aTest = aTest;
+}());
+
+
+
+
+
